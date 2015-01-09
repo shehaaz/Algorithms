4190434cf2296b17b06a5b95a72ea12895434101
shehaaz <shehaaz@gmail.com>
2015-01-08 21:36:12 -0800

    String Manipulation example

--- src/stringManipulation/Example.java
+++ src/stringManipulation/Example.java
@@ -1,10 +1,30 @@
 package stringManipulation;
 
+
 public class Example {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	protected static String reverseString(String s) {
+		
+		String result = "";
+		
+		for(int i = s.length()-1; i>=0; i--){
+			result = result + s.charAt(i);
+		}
+		
+		return result;
+	}
+	
+	protected static String reverseStringDivideAndConquer(String s){
+		
+		char[] result = new char[s.length()];
+		int lastIndex = s.length() - 1;
+		
+		for(int i=0; i<= s.length()/2; i++){
+			result[i] = s.charAt(lastIndex - i);
+			result[lastIndex - i] = s.charAt(i);
+		}
+		
+		return new String(result);
 	}
 
 }
---
+++ src/stringManipulation/ExampleTest.java
@@ -0,0 +1,25 @@
+package stringManipulation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void testReverseString() {
+		String input = "example";
+		String output = Example.reverseString(input);
+		String result = "elpmaxe";
+		assertTrue(result.equals(output));
+	}
+	
+	@Test
+	public void testReverseStringDivideAndConquer() {
+		String input = "example";
+		String output = Example.reverseStringDivideAndConquer(input);
+		String result = "elpmaxe";
+		assertTrue("result: " + result + " output: " + output, result.equals(output));
+	}
+
+}
